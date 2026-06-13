1e18af07799d2198770a32579e977cbdc6a86b65
Vlad_Hv <vladhv15102009@gmail.com>
2026-06-13 20:50:55 +0300

    Week 1, Block 3

--- Task_1.go
+++ Task_1.go
@@ -3,36 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	const appName = "Go task manager"
-	var name string
-	var age int
-	var price float64
-	var isStudent bool
+	const password = "VAD216"
 
-	fmt.Println("Application:", appName)
+	var userPassword string
 
-	fmt.Printf("Type of name %T\n", name)
-	fmt.Printf("Type of age %T\n", age)
-	fmt.Printf("Type of price %T\n", price)
-	fmt.Printf("Type of isStudent %T\n", isStudent)
+	fmt.Println("Write your password:")
+	_, err := fmt.Scanln(&userPassword)
 
-	fmt.Println("Zero value for string:", name)
-	fmt.Println("Zero value for int:", age)
-	fmt.Println("Zero value for float64:", price)
-	fmt.Println("Zero value for bool:", isStudent)
-
-	name = "Vlad"
-	age = 16
-	price = 120.50
-	isStudent = true
-
-	fmt.Println("Next Task:")
-
-	fmt.Println("Name:", name)
-	fmt.Println("Age:", age)
-	fmt.Println("Price:", price)
-	fmt.Println("is Student:", isStudent)
-
-	fmt.Printf("Memorizing, type of tse isStudent is %T\n", isStudent)
+	if err != nil {
+		fmt.Println("Passsword must include laters and numbers")
+		return
+	} else if password == userPassword {
+		fmt.Println("\nAccess granted")
+	} else {
+		fmt.Println("\nAccess denied")
+	}
 
 }
