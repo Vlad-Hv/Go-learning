92d340dba9e26d86e6016b9e092097b76e28e714
Vlad_Hv <vladhv15102009@gmail.com>
2026-06-28 00:25:49 +0300

    Mini-Project

--- Task_1.go
+++ Task_1.go
@@ -3,19 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var allNumbers [5]int
-	result := 0
+	var marks [5]int
+	var sum int
+	var avarageCount int
+	var biggestMark int
 
-	for i := 0; i < len(allNumbers); i++ {
-		fmt.Print("\nWrite number: ")
-		fmt.Scanln(&allNumbers[i])
+	//fmt.Println(len(marks))
+	for i := 0; i < len(marks); i++ {
+		fmt.Print("\n Write ", i+1, " mark: ")
+		_, err := fmt.Scanln(&marks[i])
+
+		//if err != nil || marks[i] < 1 || marks[i] > 10{
+		//fmt.Print("Uncorrrect input. Please, write correctly")
+		if err != nil {
+			fmt.Println("Incorrect input")
+			return
+		}
+		for marks[i] < 1 || marks[i] > 10 {
+
+			fmt.Println("Uncorrrect input. Please, write correctly")
+			fmt.Print("\n Write ", i+1, " mark: ")
+			_, err = fmt.Scanln(&marks[i])
+
+		}
+
 	}
 
-	for _, number := range allNumbers {
-		fmt.Println(result, "+", number, "=", result+number)
-		result += number
-
+	for _, value := range marks {
+		sum += value
 	}
 
-	fmt.Print(result)
+	avarageCount = sum / len(marks)
+	biggestMark = 0
+
+	for _, value := range marks {
+		if biggestMark < value {
+			biggestMark = value
+		}
+	}
+
+	fmt.Println("\n\n\nStudent REPORT\n\nGrades:\n", marks, "\n\nAvarage:\n", avarageCount, "\n\nHighest grade:\n", biggestMark)
 }
