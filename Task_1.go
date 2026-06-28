0b4a4c4a673db2f6319f388f73919268c8458d0e
Vlad_Hv <vladhv15102009@gmail.com>
2026-06-28 22:00:33 +0300

    mini-project (learned type conversations)

--- Task_1.go
+++ Task_1.go
@@ -3,15 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	marks := [5]int{2, 5, 8, 1, 10}
+	var Prices [5]int
 	var sum int
+	var theBiggest int
 
-	for _, value := range marks {
-		sum += value
+	for i := 0; i < len(Prices); i++ {
+		fmt.Print("\n\nWrite ", i+1, " number: ")
+		_, err := fmt.Scanln(&Prices[i])
+
+		if err != nil {
+			fmt.Println("Incorrect input")
+			return
+		}
+
+		for Prices[i] <= 0 {
+			fmt.Print("\n\nIncorrect price, please, try again: ")
+			fmt.Scanln(&Prices[i])
+		}
+		sum += Prices[i]
 	}
 
-	avarage := float64(sum) / float64(len(marks))
+	var average float64 = float64(sum) / float64(len(Prices))
 
-	fmt.Println(sum, avarage)
+	for i := 0; i < len(Prices); i++ {
+		if theBiggest < Prices[i] {
+			theBiggest = Prices[i]
+		}
+	}
+	//Kai, this comment for you, I know, that there are may be better options: average := float64(sum) / float64(len(Prices)) and for _, value := range Prices{ .. theBiggest < value .. theBiggest = value, I chose another options just for remember, that may be other options :)
+
+	fmt.Println("\n\n\nShoping Report\n\nPrices:", Prices, "\n\nTotal:", sum, "\n\nAverage:", average, "\n\nMost expensive:", theBiggest)
 
 }
