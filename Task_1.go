f138da5df97d192579abeb8e78171821e41f352a
Vlad_Hv <vladhv15102009@gmail.com>
2026-06-30 14:30:16 +0300

    Task 3

--- Task_1.go
+++ Task_1.go
@@ -3,48 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	first, second := gettingVariables()
-	userChooise(first, second)
+	username, userAge, err := printUserInfo()
+
+	if err != nil {
+		return
+	}
+
+	isAdult := isAdult(userAge)
+
+	printInfo(username, userAge, isAdult)
 }
 
-func gettingVariables() (int, int) {
-	var first int
-	var second int
-	fmt.Print("Write two numbers to calculate it: ")
-	fmt.Scanln(&first, &second)
+func printUserInfo() (string, int, error) {
+	var username string
+	var userAge int
 
-	return first, second
+	fmt.Println("Write your name:")
+	fmt.Scanln(&username)
+	fmt.Println("Well, write your age:")
+	_, err := fmt.Scanln(&userAge)
+
+	return username, userAge, err
 }
 
-func plus(first int, second int) int {
-	return first + second
+func isAdult(userAge int) bool {
+	return userAge >= 18
 }
 
-func minus(first, second int) int {
-	return first - second
-}
-
-func umnoshit(first, second int) int {
-	return first * second
-}
-
-func userChooise(first, second int) {
-	var option int
-	fmt.Println("What you do wanna do with your numbers? '+' - 1, '-' - 2, '*' - 3")
-	fmt.Scanln(&option)
-
-	switch option {
-
-	case 1:
-		fmt.Println(first, "+", second, "=", plus(first, second))
-
-	case 2:
-		fmt.Println(first, "-", second, "=", minus(first, second))
-
-	case 3:
-		fmt.Println(first, "*", second, "=", umnoshit(first, second))
-
-	default:
-		fmt.Println("Incorrect input")
+func printInfo(username string, userAge int, isAdult bool) {
+	fmt.Println("user", username, "is", userAge, "years old")
+	if !isAdult {
+		fmt.Println("Access denied")
+	} else {
+		fmt.Println("Access granted")
 	}
 }
