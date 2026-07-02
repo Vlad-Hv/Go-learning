68c86149af2a2502f14b35e9704526b426d7255f
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-02 23:04:08 +0300

    Task 3

--- Task_1.go
+++ Task_1.go
@@ -1,36 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	first, second, err := getNumbers()
+
+	fmt.Println("\nAuthorise please\n ")
+
+	name, err := nameGetter()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	result := diline(first, second)
+	age, err := ageGetter()
 
-	fmt.Println(result)
-}
-
-func getNumbers() (int, int, error) {
-	var first int
-	var second int
-
-	fmt.Print("Enter two numbers to diline: ")
-	fmt.Scanln(&first, &second)
-
-	if second == 0 {
-		return 0, 0, fmt.Errorf("Incorrect number: %d", second)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	return first, second, nil
+	fmt.Println("\n\nYour name:", name, "\nYour age:", age)
+
 }
 
-func diline(first, second int) float64 {
-	return float64(first) / float64(second)
+func nameGetter() (string, error) {
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	if name == "" {
+		return "", errors.New("variable name is empty")
+	}
+
+	return name, nil
+}
+
+func ageGetter() (int, error) {
+	var age int
+	fmt.Print("\nWell, Enter your age: ")
+	fmt.Scanln(&age)
+
+	if age < 0 {
+		return 0, fmt.Errorf("Incorrect age: %d", age)
+	}
+
+	return age, nil
 }
