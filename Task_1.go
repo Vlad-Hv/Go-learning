70159a32cc57e44e4d186f7f6082a8fdff385b0e
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-02 21:49:34 +0300

    Task 1    Week 2, Block 2

--- Task_1.go
+++ Task_1.go
@@ -1,85 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	i := question()
+	age, err := getAge()
 
-	if i != "y" && i != "n" {
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	for i != "n" {
-		option := printMenu()
-		output(option)
-		i = question()
-		if i != "y" && i != "n" {
-			return
-		}
+	ageCheker(age)
+
+}
+
+func getAge() (int, error) {
+	var userAge int
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&userAge)
+
+	if userAge < 0 {
+		return 0, errors.New("incorrect age")
 	}
 
-	fmt.Println("Goodbye!")
+	return userAge, nil
 }
 
-func printMenu() int {
-	var option int
-	fmt.Println("======Calculator======\n\n1 - '+'\n2 - '-'\n3 - '*'\n4 - '/'")
-	fmt.Scanln(&option)
-
-	return option
-}
-
-func inputNum() (int, int) {
-	var first int
-	var second int
-
-	fmt.Println("Enter two numbers:")
-	fmt.Scanln(&first, &second)
-
-	return first, second
-}
-
-func plus(first, second int) int {
-	return first + second
-}
-
-func minus(first, second int) int {
-	return first - second
-}
-
-func myltiple(first int, second int) int {
-	return first * second
-}
-
-func dilit(first int, second int) float64 {
-	return float64(first) / float64(second)
-}
-
-func output(option int) {
-	first, second := inputNum()
-	switch option {
-
-	case 1:
-		fmt.Println(first, "+", second, "=", plus(first, second))
-
-	case 2:
-		fmt.Println(first, "-", second, "=", minus(first, second))
-
-	case 3:
-		fmt.Println(first, "*", second, "=", myltiple(first, second))
-
-	case 4:
-		fmt.Println(first, "/", second, "=", dilit(first, second))
-
-	default:
-		fmt.Println("Incorrect enter")
+func ageCheker(age int) {
+	if age >= 0 {
+		fmt.Println("Age accepted")
 	}
-}
-
-func question() string {
-	var i string
-	fmt.Println("Do you want to continue? (y/n)")
-	fmt.Scanln(&i)
-
-	return i
 }
