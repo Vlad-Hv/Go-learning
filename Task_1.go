1954378c4b736ef14a0db8d37752323774f41317
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-03 17:04:41 +0300

    Extrra Mini Project

--- Task_1.go
+++ Task_1.go
@@ -6,82 +6,115 @@ import (
 )
 
 func main() {
-	const name string = "Vlad"
-	const age int = 16
-	const password string = "vlad16ag"
+	var balance float64 = 1000
 
-	err := checkAge(name, age)
+	for {
+		fmt.Println("\n===== Mini Bank =====\n\n ")
+		option, err := menuOption()
 
-	if err != nil {
-		fmt.Println(err)
-		return
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if option == 4 {
+			break
+		}
+
+		switch option {
+
+		case 1:
+			showBalance(balance)
+
+		case 2:
+			deposit, err := depositMoney(balance)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			balance = deposit
+			success()
+
+		case 3:
+			result, err := withdrawMoney(balance)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			balance = result
+			success()
+
+		default:
+			fmt.Println("\nWrong option, try again")
+			continue
+
+		}
 	}
 
-	err = checkPassword(password)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("Welcome, Vlad :)")
+	fmt.Println("\n\nGoodbye :)")
 
 }
 
-func checkPassword(password string) error {
-	var userPassword string
-
-	fmt.Print("Enter your password down\n>>> ")
-	fmt.Scanln(&userPassword)
-
-	if len(userPassword) < 8 {
-		return fmt.Errorf("too short password: %d", len(userPassword))
-	}
-
-	if userPassword != password {
-		return fmt.Errorf("incorrect password: %q", userPassword)
-	}
-
-	return nil
-}
-
-func checkName(name string) error {
-
-	var userName string
-	fmt.Print("Enter your Name: ")
-	fmt.Scanln(&userName)
-
-	if userName == "" {
-		return errors.New("the 'name' mustn't be empty")
-	}
-
-	if userName != name {
-		return errors.New("wrong name")
-	}
-
-	return nil
-}
-
-func checkAge(name string, age int) error {
-
-	err := checkName(name)
+func getUserAnswer() (int, error) {
+	var userAnswer int
+	fmt.Print("Your budjet = 1000$\nDo you want to start? (1 - y/n - 2): ")
+	_, err := fmt.Scanln(&userAnswer)
 
 	if err != nil {
-		return fmt.Errorf("cannot validate age, reason: %w", err)
+		return 0, errors.New("Incorrect input type")
 	}
 
-	var userAge int
-	fmt.Println("Enter your age")
-	fmt.Scanln(&userAge)
+	return userAnswer, nil
+}
 
-	if userAge < 0 {
-		return fmt.Errorf("incorrect age: %d", userAge)
+func menuOption() (int, error) {
+	var option int
+	fmt.Print("\n1. Show balance\n2. Deposit money\n3. Withdraw money\n4. Exit\n\nChoose option: ")
+	_, err := fmt.Scanln(&option)
+
+	if err != nil {
+		return 0, errors.New("entered option must have integer type")
 	}
 
-	if userAge != age {
-		return errors.New("incorrect age input")
+	return option, nil
+}
+
+func success() {
+	fmt.Println("Balance changed successfully!")
+}
+
+func showBalance(balance float64) {
+	fmt.Println("Current balance:", balance)
+}
+
+func depositMoney(balance float64) (float64, error) {
+	var depSum float64
+	fmt.Print("Enter sum which you wanna deposit: ")
+	fmt.Scanln(&depSum)
+
+	if depSum <= 0 {
+		return 0, fmt.Errorf("incorrect deposit sum: %v", depSum)
 	}
 
-	return nil
+	return depSum + balance, nil
+}
 
+func withdrawMoney(balance float64) (float64, error) {
+	var withdraw float64
+	fmt.Print("\n\nEnte Withdraw sum: ")
+	fmt.Scanln(&withdraw)
+
+	if withdraw <= 0 {
+		return 0, fmt.Errorf("withdraw sum must be positive: %v", withdraw)
+	}
+
+	if withdraw > balance {
+		return 0, errors.New("not enough money")
+	}
+
+	return balance - withdraw, nil
 }
