fddd3860190eda6084dc080dd018d5685fa990fd
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-06 00:53:41 +0300

    Task 2

--- Task_1.go
+++ Task_1.go
@@ -3,13 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	languages := []string{"Golang", "Python", "Java", "Rust"}
-	printLangs(languages)
-
+	numbers := getSlice()
+	fmt.Println(getTotal(numbers))
 }
 
-func printLangs(array []string) {
-	for _, languages := range array {
-		fmt.Print(languages, " ")
+func getSlice() []int {
+	var numbers []int
+	for {
+		var number int
+		fmt.Print("\nEnter number to add it: ")
+		_, err := fmt.Scanln(&number)
+
+		if err != nil {
+			fmt.Println("you left the adding process")
+			break
+		}
+
+		numbers = append(numbers, number)
 	}
+	return numbers
+}
+
+func getTotal(numbers []int) int {
+	var total int
+
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
 }
