71ba11a88052352556da57b0dbb96b9d480dd601
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-05 22:19:17 +0300

    Task 3

--- Task_1.go
+++ Task_1.go
@@ -3,39 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	firstArray := [4]int{1, 2, 3, 4}
+	secondArray := change(firstArray)
 
-	array := getArray()
-
-	fmt.Print(getSum(array))
-
+	fmt.Println(firstArray, "\n", secondArray)
 }
 
-func getSum(array [5]int) int {
-
-	var total int
-
-	for _, number := range array {
-
-		total += number
-
-	}
-
-	return total
-
-}
-
-func getArray() [5]int {
-
-	var array [5]int
-
-	for i := 0; i < 5; i++ {
-
-		fmt.Print("\nEnter ", i+1, " number: ")
-
-		fmt.Scanln(&array[i])
-
-	}
-
+func change(array [4]int) [4]int {
+	array[0] = 999
 	return array
-
 }
