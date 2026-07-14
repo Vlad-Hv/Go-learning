8d964e8058441cb0cf47aa6f70b20ec10e604359
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-15 00:28:08 +0300

    Last progect to final memorizing map and slice topics

--- Task_1.go
+++ Task_1.go
@@ -6,263 +6,178 @@ import (
 )
 
 func main() {
-	productList := productsMap()
-	history := makeSlice()
-	statistic := statistick()
-
+	commandList := commandList()
+	users := activeUsers()
+	eventHandler := infoMap()
+	userCounter := make(map[string]int)
+	var command string
+	var err error
+	var history []string
+	var username string
 	for {
-		option, err := getOption()
+		users, history, command, username, err = checkerCommand(commandList, users, eventHandler, history, userCounter)
 
 		if err != nil {
 			fmt.Println(err)
-			statistic["Errors count"] += 1
+			eventHandler["Rejected actions"] += 1
 			continue
 		}
 
-		if option == 7 {
-			fmt.Println("\nGoodBye!")
+		if len(history) > 5 {
+			history = history[1:]
+		}
+
+		userCounter[username] += 1
+
+		if command == "report" {
 			break
 		}
 
-		switch option {
-		case 1:
-			printProducts(productList)
+	}
+}
 
-		case 2:
-			err, history = addProduct(productList, history, statistic)
-			if err != nil {
-				fmt.Println(err)
-				statistic["Errors count"] += 1
-				continue
-			}
+func activeUsers() []string {
+	var users []string
+	return users
+}
 
-		case 3:
-			err, history = sellProduct(productList, history, statistic)
+func getInfo(commands []string) (string, string, error) {
+	var user string
+	var command string
+	var checker int
+	fmt.Print("\nEnter username and command: ")
+	fmt.Scanln(&user, &command)
 
-			if err != nil {
-				fmt.Println(err)
-				statistic["Errors count"] += 1
-				continue
-			}
-
-		case 4:
-			err, history = restockProduct(productList, history, statistic)
-
-			if err != nil {
-				fmt.Println(err)
-				statistic["Errors count"] += 1
-				continue
-			}
-
-		case 5:
-			printHistory(history)
-
-		case 6:
-			printStatistic(statistic)
-		default:
-			fmt.Println("Invalid menu option")
-			statistic["Errors count"] += 1
-		}
-
-		if len(history) > 10 {
-			history = history[1:]
+	for i := 0; i < len(commands); i++ {
+		if command == commands[i] {
+			checker += 1
+			break
 		}
 	}
 
-}
-
-func productsMap() map[string]int {
-	productList := make(map[string]int)
-	return productList
-}
-
-func getOption() (int, error) {
-	var option int
-	fmt.Print("===== Warehouse Manager =====\n1. Show products\n2. Add product\n3. Sell product\n4. Restock product\n5. Show operation history\n6. Show statistics\n7. Exit\nChoose option: ")
-	_, err := fmt.Scanln(&option)
-
-	if err != nil {
-		return 0, errors.New("invalid option type")
+	if checker == 0 {
+		return "", "", errors.New("invalid command")
 	}
 
-	return option, nil
+	return user, command, nil
 }
 
-func printProducts(prodList map[string]int) {
-	if len(prodList) > 0 {
-		fmt.Println("\n\n===== Products =====")
-		for productName, amount := range prodList {
-			fmt.Println(productName, ": ", amount)
+func checkerCommand(comList []string, users []string, eventCheck map[string]int, history []string, uC map[string]int) ([]string, []string, string, string, error) {
+	username, command, err := getInfo(comList)
+	var message string
+
+	if username == "report" {
+		command = username
+	}
+
+	if err != nil {
+		return users, history, "", "", fmt.Errorf("error: %w", err)
+	}
+
+	switch command {
+	case "login":
+		users, history, err = userLogin(users, username, eventCheck, history)
+
+		if err != nil {
+			return users, history, "", "", err
 		}
-		fmt.Println("")
 
-	} else {
-		fmt.Println("\nWarehouse is empty")
+		return users, history, command, username, nil
+	case "logout":
+		users, history, err = userLogout(users, username, eventCheck, history)
+
+		if err != nil {
+			return users, history, "", "", err
+		}
+
+		return users, history, command, username, nil
+	case "buy":
+		message, history, err = userBuy(users, username, eventCheck, history)
+
+		if err != nil {
+			return users, history, "", "", err
+		}
+		fmt.Println(message)
+
+	case "report":
+		report(users, eventCheck, history, uC)
+		return users, history, command, username, nil
 	}
+	return users, history, command, username, nil
+
 }
 
-func addProduct(prodList map[string]int, history []string, stat map[string]int) (error, []string) {
-	var productName string
-	var quantity int
-
-	fmt.Print("Enter product name: ")
-	fmt.Scanln(&productName)
-	fmt.Print("Enter quantity: ")
-	_, err := fmt.Scanln(&quantity)
-
-	if err != nil {
-		return errors.New("invalid quantity type"), history
-	}
-
-	if productName == "" {
-		return errors.New("name must not be empty"), history
-	}
-
-	if quantity <= 0 {
-		return fmt.Errorf("quantity cannot be less than zero or zero as your input %d", quantity), history
-	}
-
-	_, ok := prodList[productName]
-
-	if ok == true {
-		return errors.New("Product already exists"), history
-	}
-
-	prodList[productName] = quantity
-
-	addMessage := fmt.Sprintf("Added %q: +%d", productName, quantity)
-	history = append(history, addMessage)
-	stat["Products added"] += 1
-
-	return nil, history
+func commandList() []string {
+	commands := []string{"login", "logout", "buy", "report"}
+	return commands
 }
 
-func sellProduct(prodList map[string]int, history []string, stat map[string]int) (error, []string) {
-	productName, quantity, err := askProductAndQuantity(prodList)
-
-	if err != nil {
-		return err, history
+func userLogin(userList []string, userName string, eventCheck map[string]int, history []string) ([]string, []string, error) {
+	if len(userList) != 0 {
+		for _, user := range userList {
+			if user == userName {
+				eventCheck["Rejected actions"] += 1
+				return userList, history, errors.New("duplicate login")
+			}
+		}
 	}
 
-	//_, ok := prodList[productName]
+	userList = append(userList, userName)
+	eventCheck["Successful actions"] += 1
 
-	/*if !ok {
-		return errors.New("Product not found")
+	message := fmt.Sprintf("%q logget in succesfully", userName)
+	history = append(history, message)
+	fmt.Println("user loged successfully!")
+	return userList, history, nil
+}
+
+func userLogout(userList []string, username string, event map[string]int, history []string) ([]string, []string, error) {
+	if len(userList) == 0 {
+		event["Rejected actions"] += 1
+		return userList, history, errors.New("userlist is clear")
+	}
+
+	/*for _, user := range userList{
+		if user == username
 	}*/
 
-	if prodList[productName] < quantity {
-		return fmt.Errorf("Not enough %d stock", quantity-prodList[productName]), history
-	}
-
-	prodList[productName] = prodList[productName] - quantity
-
-	if prodList[productName] == 0 {
-		delete(prodList, productName)
-		stat["Products removed"] += 1
-		removeHistoryMessage := fmt.Sprintf("Removed %q after selling all units", productName)
-		history = append(history, removeHistoryMessage)
-	}
-
-	fmt.Println("Product sold successfully")
-
-	sellHistoryMessage := fmt.Sprintf("Sold %q: -%d", productName, quantity)
-	history = append(history, sellHistoryMessage)
-	stat["Products sold"] += 1
-
-	return nil, history
-}
-
-func askProductAndQuantity(prodList map[string]int) (string, int, error) {
-	var productName string
-	var quantity int
-
-	fmt.Print("Enter product name: ")
-	fmt.Scanln(&productName)
-
-	_, ok := prodList[productName]
-
-	if !ok {
-		return "", 0, errors.New("Product not found")
-	}
-	fmt.Print("Enter product quantity: ")
-	_, err := fmt.Scanln(&quantity)
-
-	if err != nil {
-		return "", 0, errors.New("\nincorrect quantity type")
-	}
-
-	if productName == "" {
-		return "", 0, errors.New("\ninput must not be empty")
-	}
-
-	if quantity <= 0 {
-		return "", 0, errors.New("\nInvalid quantity")
-	}
-
-	return productName, quantity, nil
-}
-
-/*func check(err error) {
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}*/
-
-func restockProduct(prodList map[string]int, history []string, stat map[string]int) (error, []string) {
-	productName, quantity, err := askProductAndQuantity(prodList)
-
-	if err != nil {
-		return fmt.Errorf("cannot restore nothing, reason %w", err), history
-	}
-
-	/*_, ok := prodList[productName]
-
-	if !ok {
-		return errors.New("Product not found")
-	}*/
-
-	prodList[productName] = prodList[productName] + quantity
-	fmt.Println("Product restocked successfully")
-
-	restockHistoryMessage := fmt.Sprintf("Restocked %q +%d", productName, quantity)
-	history = append(history, restockHistoryMessage)
-	stat["Products restocked"] += 1
-	return nil, history
-}
-
-func makeSlice() []string {
-	var slice []string
-	return slice
-}
-
-func statistick() map[string]int {
-	statistic := map[string]int{
-		"Products added":     0,
-		"Products sold":      0,
-		"Products restocked": 0,
-		"Products removed":   0,
-		"Errors count":       0,
-	}
-	return statistic
-}
-
-func printStatistic(stat map[string]int) {
-	fmt.Println("\n===== Statistics =====")
-	for productNames, quantity := range stat {
-		fmt.Println(productNames, ": ", quantity)
-	}
-	fmt.Println("")
-}
-
-func printHistory(history []string) {
-	if len(history) > 0 {
-		fmt.Println("\n===== Operation History =====")
-		for _, amount := range history {
-			fmt.Println(": ", amount)
+	for i := 0; i < len(userList); i++ {
+		if userList[i] == username {
+			userList = append(userList[:i], userList[i+1:]...)
+			fmt.Println("user logged out successfully!")
+			message := fmt.Sprintf("%q logget out succesfully", username)
+			history = append(history, message)
+			event["Successful actions"] += 1
+			return userList, history, nil
 		}
-		fmt.Println()
-	} else {
-		fmt.Println("History is empty")
 	}
+	event["Rejected actions"] += 1
+	return userList, history, errors.New("invalid logout")
+}
+
+func userBuy(userList []string, username string, event map[string]int, history []string) (string, []string, error) {
+	var buyMessage string
+	for _, user := range userList {
+		if user == username {
+			buyMessage = "purchase accepted"
+			event["Successful actions"] += 1
+			message := fmt.Sprintf("%q bought successfully!", username)
+			history = append(history, message)
+			return buyMessage, history, nil
+		}
+	}
+	event["Rejected actions"] += 1
+	return "", history, fmt.Errorf("user(%q) is not logged in", username)
+}
+
+func report(users []string, info map[string]int, history []string, uC map[string]int) {
+	fmt.Println("\n\n", users, "\n", info, "\n\n", history, "\n\n", uC)
+}
+
+func infoMap() map[string]int {
+	info := map[string]int{
+		"Successful actions": 0,
+		"Rejected actions":   0,
+	}
+	return info
 }
