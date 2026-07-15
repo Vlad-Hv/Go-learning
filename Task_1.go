e64240a5d697c761dfa4e6ce664b7b1deb2ac64a
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-16 00:57:56 +0300

    Task 2

--- Task_1.go
+++ Task_1.go
@@ -2,26 +2,43 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name      string
-	Age       int
-	Country   string
-	IsStudent bool
+type Product struct {
+	Name        string
+	Price       float64
+	Amount      int
+	IsAvailable bool
 }
 
 func main() {
-
-	studentBeta := User{
-		Name:      "Vlad",
-		Age:       16,
-		Country:   "Belarus",
-		IsStudent: true,
+	name, price, option := getInfo()
+	product := Product{
+		Name:        name,
+		Price:       price,
+		Amount:      1,
+		IsAvailable: true,
 	}
 
-	fmt.Println(studentBeta.Name, "\n", studentBeta.Age, "\n", studentBeta.Country, "\n", studentBeta.IsStudent)
+	fmt.Println(product)
 
-	studentBeta.Age = 20
-	studentBeta.IsStudent = false
+	if option == 1 {
+		product.Amount = 0
+		product.IsAvailable = false
+		fmt.Println("Product is not available")
+	} else {
+		fmt.Println("Product is available")
+	}
 
-	fmt.Println(studentBeta.Name, "\n", studentBeta.Age, "\n", studentBeta.Country, "\n", studentBeta.IsStudent)
+}
+
+func getInfo() (string, float64, int) {
+	var name string
+	var price float64
+	var option int
+
+	fmt.Println("Enter product name and price:")
+	fmt.Scanln(&name, &price)
+	fmt.Print("Do you want to buy smth y-1, n-2: ")
+	fmt.Scanln(&option)
+
+	return name, price, option
 }
