830b0983fa89d70740ab4b5223859fb5e29e5686
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-16 16:10:31 +0300

    Task 3

--- Task_1.go
+++ Task_1.go
@@ -2,43 +2,64 @@ package main
 
 import "fmt"
 
-type Product struct {
-	Name        string
-	Price       float64
-	Amount      int
-	IsAvailable bool
+type Character struct {
+	Name       string
+	Health     int
+	Level      int
+	GoldAmount int
+	IsAlive    bool
 }
 
 func main() {
-	name, price, option := getInfo()
-	product := Product{
-		Name:        name,
-		Price:       price,
-		Amount:      1,
-		IsAvailable: true,
+	var name string
+	fmt.Println("Enter hero name: ")
+	fmt.Scanln(&name)
+
+	startCharacter := Character{
+		Name:       name,
+		Health:     100,
+		GoldAmount: 0,
+		Level:      1,
+		IsAlive:    true,
 	}
 
-	fmt.Println(product)
+	actions := actionSlice()
 
-	if option == 1 {
-		product.Amount = 0
-		product.IsAvailable = false
-		fmt.Println("Product is not available")
-	} else {
-		fmt.Println("Product is available")
+	fmt.Println(startCharacter)
+
+	for i := 0; (len(actions) - 1) > i; i++ {
+
+		switch i {
+		case 0:
+			fmt.Println(actions[i])
+			startCharacter.Health -= 35
+
+		case 1:
+			fmt.Println(actions[i])
+			startCharacter.GoldAmount += 50
+
+		case 2:
+			fmt.Println(actions[i])
+			startCharacter.Level += 1
+
+		case 3:
+			fmt.Println(actions[i])
+			startCharacter.Health -= 70
+		}
+
+		if startCharacter.Health <= 0 {
+			startCharacter.Health = 0
+			startCharacter.IsAlive = false
+			fmt.Print(actions[4], "\n ")
+		}
+
 	}
+
+	fmt.Println(startCharacter)
 
 }
 
-func getInfo() (string, float64, int) {
-	var name string
-	var price float64
-	var option int
-
-	fmt.Println("Enter product name and price:")
-	fmt.Scanln(&name, &price)
-	fmt.Print("Do you want to buy smth y-1, n-2: ")
-	fmt.Scanln(&option)
-
-	return name, price, option
+func actionSlice() []string {
+	actions := []string{"\nYour hero got 35 damage", "\nYour hero found 50 golds", "\nYour hero level up for 1 level", "\nYour hero got 70 damage", "\nYour hero died"}
+	return actions
 }
