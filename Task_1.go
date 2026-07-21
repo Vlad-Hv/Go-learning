468874a618e188f8ae597756ca89d1a4d96da1d7
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-21 23:34:19 +0300

    Task 1 Block 1.3 Week 3

--- Task_1.go
+++ Task_1.go
@@ -1,145 +1,59 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"math/rand"
 )
 
-type Adress struct {
-	City       string
-	Street     string
-	HomeNumber float64
-}
-
-type Recipient struct {
-	Name        string
-	PhoneNumber string
-	Adress      Adress
-}
-
-type Parcel struct {
-	ID        int
-	Weight    float64
-	Cost      float64
-	Recipient Recipient
-	IsPost    bool
+type Book struct {
+	Title  string
+	Author string
+	Pages  int
+	IsRead bool
 }
 
 func main() {
-	var cost float64 = 1500.50
-	name, phoneNumber, adress, err := getRecipientInfo()
-	id := getId()
+	var readBooks int
+	books := []Book{
+		{
+			Title:  "Brick",
+			Author: "Daniil",
+			Pages:  200,
+			IsRead: false,
+		},
 
-	if err != nil {
-		fmt.Println(err)
-		return
+		{
+			Title:  "Death Planet",
+			Author: "NN",
+			Pages:  3234,
+			IsRead: false,
+		},
+
+		{
+			Title:  "45 mind picture",
+			Author: "Daba setre",
+			Pages:  34,
+			IsRead: false,
+		},
+
+		{
+			Title:  "Garry Potter",
+			Author: "Ronald hfhfh",
+			Pages:  316,
+			IsRead: false,
+		},
 	}
 
-	changedPhoneNumber := fmt.Sprintf("+%d", phoneNumber)
-	weight, err := getParcelInfo()
-
-	if err != nil {
-		fmt.Println(err)
-		return
+	for _, book := range books {
+		fmt.Println(book.Title)
 	}
 
-	if weight > 10 {
-		cost += cost / 2
+	books[rand.Intn(len(books))].IsRead = true
+
+	for _, book := range books {
+		if book.IsRead == true {
+			readBooks += 1
+		}
 	}
-
-	recipient := Recipient{
-		Name:        name,
-		PhoneNumber: changedPhoneNumber,
-		Adress:      adress,
-	}
-
-	parcel := Parcel{
-		ID:        id,
-		Weight:    weight,
-		Cost:      cost,
-		Recipient: recipient,
-		IsPost:    false,
-	}
-
-	printParcelInfo(parcel)
-	parcel = sentParcel(parcel)
-	printParcelInfo(parcel)
-}
-
-func getRecipientInfo() (string, int, Adress, error) {
-	var city string
-	var street string
-	var homeNumber float64
-	var name string
-	var phoneNumber int
-	fmt.Println("Hello, nice to meet you in our interet market!\nCould you enter your name ant phone number please: ")
-	fmt.Scanln(&name)
-	_, err := fmt.Scanln(&phoneNumber)
-
-	if err != nil {
-		return "", 0, Adress{}, errors.New("incorrect input")
-	}
-
-	fmt.Println("\nWell enter your adress(city, street, nome number) to get your recepie: ")
-	fmt.Scanln(&city)
-	fmt.Scanln(&street)
-	_, err = fmt.Scanln(&homeNumber)
-
-	if err != nil {
-		return "", 0, Adress{}, errors.New("invalid input")
-	}
-
-	if name == "" || city == "" || street == "" {
-		return "", 0, Adress{}, errors.New("your info mustnot be empty")
-	}
-
-	adress := Adress{
-		City:       city,
-		Street:     street,
-		HomeNumber: homeNumber,
-	}
-
-	return name, phoneNumber, adress, nil
-}
-
-func getParcelInfo() (float64, error) {
-	var weight float64
-	fmt.Println("\nEnter parcel weight:")
-	_, err := fmt.Scanln(&weight)
-
-	if err != nil || weight <= 0 {
-		return 0, errors.New("invalid weight")
-	}
-
-	return weight, nil
-}
-
-func getId() int {
-	id := rand.Intn(1000000) + 1000000
-	return id
-}
-
-func printParcelInfo(parcel Parcel) {
-	var isPost string
-	if !parcel.IsPost {
-		isPost = "Not Delivered"
-	} else {
-		isPost = "Delivered"
-	}
-	fmt.Println("\nParcel ID: ", parcel.ID)
-	fmt.Println("Weight:", parcel.Weight)
-	fmt.Println("Cost:", parcel.Cost)
-	fmt.Println("Name:", parcel.Recipient.Name)
-	fmt.Println("Number:", parcel.Recipient.PhoneNumber)
-	fmt.Println("City:", parcel.Recipient.Adress.City)
-	fmt.Println("Street:", parcel.Recipient.Adress.Street)
-	fmt.Println("Home number:", parcel.Recipient.Adress.HomeNumber)
-	fmt.Println("Status:", isPost)
-}
-
-func sentParcel(parcel Parcel) Parcel {
-	parcel.IsPost = true
-
-	return parcel
+	fmt.Println(readBooks)
 }
