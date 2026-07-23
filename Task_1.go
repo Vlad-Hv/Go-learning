5b9b72d720264a87670c42707882f971d2d07a51
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-23 13:46:12 +0300

    Mini project   Week 3, Block 1. Learned Struct, map struct, slice Struct, slice, map in struct

--- Task_1.go
+++ Task_1.go
@@ -1,65 +1,173 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-type Character struct {
+type Gamer struct {
 	Name       string
-	Health     int
 	Level      int
-	GoldAmount int
-	IsAlive    bool
+	Role       string
+	AmountGold int
+	DoneEvents []string
+}
+
+type Event struct {
+	ID           int
+	EventName    string
+	Award        int
+	MinimalLevel int
+	IsDone       bool
 }
 
 func main() {
-	var name string
-	fmt.Println("Enter hero name: ")
-	fmt.Scanln(&name)
+	gamers := getGamers()
+	events := getEvents()
+	guildStatistic := []string{}
 
-	startCharacter := Character{
-		Name:       name,
-		Health:     100,
-		GoldAmount: 0,
-		Level:      1,
-		IsAlive:    true,
-	}
+	for i := 0; i < len(events); i++ {
+		hero, event, err := getUserAnswer(gamers, events)
 
-	actions := actionSlice()
-
-	fmt.Println(startCharacter)
-
-	for i := 0; (len(actions) - 1) > i; i++ {
-
-		switch i {
-		case 0:
-			fmt.Println(actions[i])
-			startCharacter.Health -= 35
-
-		case 1:
-			fmt.Println(actions[i])
-			startCharacter.GoldAmount += 50
-
-		case 2:
-			fmt.Println(actions[i])
-			startCharacter.Level += 1
-
-		case 3:
-			fmt.Println(actions[i])
-			startCharacter.Health -= 70
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
-		if startCharacter.Health <= 0 {
-			startCharacter.Health = 0
-			startCharacter.IsAlive = false
-			fmt.Print(actions[4], "\n ")
-		}
+		eventHelper := events[event]
+		eventHelper.IsDone = true
+		events[event] = eventHelper
 
+		gamers[hero].AmountGold += events[event].Award
+		gamers[hero].DoneEvents = append(gamers[hero].DoneEvents, events[event].EventName)
+		guildStatistic = append(guildStatistic, events[event].EventName)
+		fmt.Println(gamers[hero])
 	}
 
-	fmt.Println(startCharacter)
-
+	fmt.Println(guildStatistic)
 }
 
-func actionSlice() []string {
-	actions := []string{"\nYour hero got 35 damage", "\nYour hero found 50 golds", "\nYour hero level up for 1 level", "\nYour hero got 70 damage", "\nYour hero died"}
-	return actions
+func getGamers() []Gamer {
+	gamers := []Gamer{
+		{
+			Name:       "Hero",
+			Level:      2,
+			Role:       "Damager",
+			AmountGold: 6200,
+			DoneEvents: []string{},
+		},
+
+		{
+			Name:       "Mark",
+			Level:      4,
+			Role:       "Healer",
+			AmountGold: 13990,
+			DoneEvents: []string{},
+		},
+
+		{
+			Name:       "Clara",
+			AmountGold: 680,
+			Level:      1,
+			Role:       "spirit",
+			DoneEvents: []string{},
+		},
+	}
+
+	return gamers
+}
+
+func getEvents() map[int]Event {
+	var FirstID int = rand.Intn(200) + 101
+	var SecondID int = rand.Intn(200) + 101
+	var TherdID int = rand.Intn(200) + 101
+
+	events := map[int]Event{
+		1: {
+			ID:           FirstID,
+			EventName:    "Kill the Dragon",
+			Award:        1990,
+			MinimalLevel: 3,
+			IsDone:       false,
+		},
+
+		2: {
+			ID:           SecondID,
+			EventName:    "Find and save the King",
+			Award:        2100,
+			MinimalLevel: 2,
+			IsDone:       false,
+		},
+
+		3: {
+			ID:           TherdID,
+			EventName:    "Steal wallet",
+			Award:        500,
+			MinimalLevel: 1,
+			IsDone:       false,
+		},
+	}
+
+	return events
+}
+
+func getUserAnswer(gamers []Gamer, events map[int]Event) (int, int, error) {
+	var heroName string
+	var eventNumber int
+	var counter int
+
+	for _, hero := range gamers {
+		fmt.Println("\n", hero.Name)
+	}
+
+	fmt.Print("\nChoose hero name: ")
+	fmt.Scanln(&heroName)
+
+	for i := 0; i < len(gamers); i++ {
+		if gamers[i].Name == heroName {
+			counter++
+		}
+	}
+
+	if counter == 0 {
+		return 0, 0, errors.New("invalid hero name")
+	}
+
+	for number, eventInfo := range events {
+		if !events[number].IsDone {
+			fmt.Println("\n", number, ":", eventInfo)
+		} else {
+			continue
+		}
+	}
+
+	fmt.Println("\n\nWell, choose event number: ")
+	_, err := fmt.Scanln(&eventNumber)
+
+	if err != nil {
+		return 0, 0, fmt.Errorf("event number must be int")
+	}
+
+	if eventNumber < 1 || eventNumber > len(events) {
+		return 0, 0, errors.New("invalid option")
+	}
+
+	if events[eventNumber].IsDone {
+		return 0, 0, errors.New("this task alredy taken")
+	}
+	counter = 0
+	for i := 0; i < len(gamers); i++ {
+		if gamers[i].Name == heroName {
+			counter = i
+		}
+	}
+
+	isEnough := gamers[counter].Level >= events[eventNumber].MinimalLevel
+
+	if !isEnough {
+		return 0, 0, errors.New("level is not high enough")
+	}
+
+	return counter, eventNumber, nil
 }
