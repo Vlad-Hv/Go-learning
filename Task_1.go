cb810335dddb1c7ad34c7ad0b2ef7dd3237b5715
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-24 00:02:32 +0300

    Task 1.3

--- Task_1.go
+++ Task_1.go
@@ -1,15 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	var username string = "Vlad"
-	fmt.Println(username)
+	var health int = 100
+	var healthPointer *int = &health
 
-	changeName(&username)
-	fmt.Println(username)
+	for i := 0; i < 2; i++ {
+
+		err := changeHealth(healthPointer)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Health:", health)
+	}
+
 }
 
-func changeName(username *string) {
-	*username = "Dima"
+func changeHealth(health *int) error {
+	var damage int
+
+	fmt.Println("Enter damage count")
+	_, err := fmt.Scanln(&damage)
+
+	if err != nil {
+		return errors.New("invlid type")
+	}
+
+	healthCheck := *health
+
+	if (healthCheck - damage) < 0 {
+		return errors.New("too much damage")
+	}
+
+	*health -= damage
+
+	return nil
 }
