44d6d27c93adbd7b0623b35225a938edfef51e5c
Vlad_Hv <vladhv15102009@gmail.com>
2026-07-29 00:12:20 +0300

    Task 1

--- Task_1.go
+++ Task_1.go
@@ -1,145 +1,41 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-type Player struct {
-	ID        int
-	Username  string
-	Health    int
-	Gold      int
-	Inventory []string
+type Product struct {
+	Name        string
+	Price       float64
+	Amount      int
+	IsAvailable bool
 }
 
 func main() {
-	var history []Player
-	players := createPlayers()
-	fmt.Println("Before:", players)
-	playerMap := createPlayerMap(players)
-	id := findID()
-	player := findPlayer(playerMap, id)
-	damage := findDamage()
-	err := takeDamage(player, damage)
+	product := createProduct()
+	product.PrintInfo()
+	fmt.Println("Tatal price:", product.TotalPrice())
+	fmt.Println("Is it in stock:", product.IsInStock()) //Kai, is it good to write like this? or it''s better to initilize variable first and print ..., variableName
+}
 
-	if err != nil {
-		fmt.Println(err)
-		return
+func createProduct() Product {
+	return Product{
+		Name:        "IPhone",
+		Price:       1999.99,
+		Amount:      10,
+		IsAvailable: true,
 	}
-
-	gold, item := findGoldAndItem()
-
-	addReward(player, gold, item)
-	addHistory(&history, *player)
-
-	id = 000
-	player = findPlayer(playerMap, id)
-	addReward(player, gold, item)
-
-	fmt.Println("After:", players, "\nHistory:", history)
 }
 
-func createPlayers() []Player {
-	players := []Player{
-		{
-			ID:        101,
-			Username:  "Vlad",
-			Health:    100,
-			Gold:      120,
-			Inventory: []string{"Axe", "Hammer"},
-		},
-
-		{
-			ID:        164,
-			Username:  "Vadim",
-			Health:    100,
-			Gold:      115,
-			Inventory: []string{"Gun", "Bullet"},
-		},
-
-		{
-			ID:        189,
-			Username:  "Dima",
-			Health:    100,
-			Gold:      1000,
-			Inventory: []string{"Cabel"},
-		},
-	}
-
-	return players
+func (product Product) PrintInfo() {
+	fmt.Println("====INFO====")
+	fmt.Println("Name:", product.Name)
+	fmt.Println("Price:", product.Price)
+	fmt.Println("Amount:", product.Amount)
 }
 
-func createPlayerMap(players []Player) map[int]*Player {
-	playerMap := make(map[int]*Player)
-	for i := range players {
-		playerMap[players[i].ID] = &players[i]
-	}
-	return playerMap
+func (product Product) TotalPrice() float64 {
+	return product.Price * float64(product.Amount)
 }
 
-func findPlayer(playerMap map[int]*Player, ID int) *Player {
-	_, ok := playerMap[ID]
-
-	if !ok {
-		return nil
-	}
-
-	return playerMap[ID]
-}
-
-func findID() int {
-	var id int
-	fmt.Println("Enter ID:")
-	fmt.Scanln(&id)
-	return id
-}
-
-func findDamage() int {
-	var damage int
-	fmt.Println("Enter damage:")
-	fmt.Scanln(&damage)
-	return damage
-}
-
-func findGoldAndItem() (int, string) {
-	var gold int
-	var item string
-
-	fmt.Println("Enter award gold and item:")
-	fmt.Scanln(&gold, &item)
-
-	return gold, item
-}
-
-func takeDamage(player *Player, damage int) error {
-	if player == nil {
-		return errors.New("icorrect id")
-	}
-
-	if damage < 0 {
-		return errors.New("damage mustnot be less than zero")
-	}
-
-	healthDEMO := player.Health - damage
-
-	if healthDEMO < 0 {
-		player.Health = 0
-	} else {
-		player.Health -= damage
-	}
-
-	return nil
-}
-
-func addReward(player *Player, gold int, item string) {
-	if player == nil {
-		return
-	}
-	player.Gold += gold
-	player.Inventory = append(player.Inventory, item)
-}
-
-func addHistory(players *[]Player, player Player) {
-	*players = append(*players, player)
+func (product Product) IsInStock() bool {
+	return product.Amount > 0 && product.IsAvailable == true
 }
